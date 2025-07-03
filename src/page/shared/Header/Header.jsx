8f3f6ee4cb3147f14cc57127325e663d92f9f999
rahmanrizvi <rahmanrizvi422@gmail.com>
2025-07-03 12:01:25 -0700

    rahman_cv best

--- src/page/shared/Header/Header.jsx
+++ src/page/shared/Header/Header.jsx
@@ -1,10 +1,9 @@
-
 import Reveal from '../../../animation/Reveal';
 const Header = () => {
+   
     return (
         <Reveal>
-            <div
-                className='text-center p-2'>
+              <div className='text-center p-2'>
                 <h2 className='text-2xl font-semibold m-1'>MOKSODOR RAHMAN (RIZVI)</h2>
                 <p className='text-xl m-1'>"𝕀'𝕞 𝕒 𝔽𝕦𝕝𝕝-𝕊𝕥𝕒𝕔𝕜 𝕎𝕖𝕓 𝔻𝕖𝕧𝕖𝕝𝕠𝕡𝕖𝕣"</p>
             </div>
@@ -14,6 +13,3 @@ const Header = () => {
 
 export default Header;
 
-// initial={{ opacity: 0, scale: 0.5 }}
-// animate={{ opacity: 1, scale: 1 }}
-// transition={{ duration: 0.5 }}
